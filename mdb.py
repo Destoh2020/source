fc1138c788baebdd87d0595d796fd06473361c8a
Nyandika Donald <donald.nyandika@gmail.com>
2021-02-24 21:41:00 +0300

    Jsonify list to fix issues

--- mdb.py
+++ mdb.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -10,6 +10,7 @@ app.config['MYSQL_DB'] = 'projects'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
+CORS(app)
 
 
 @app.route('/login')
@@ -19,11 +20,11 @@ def login():
 
     if username is None:
         response_message = [False, 'Username was not provided']
-        return response_message
+        return jsonify(response_message)
 
     if password is None:
         response_message = [False, 'password was not provided']
-        return response_message
+        return jsonify(response_message)
 
     query = 'SELECT ID, FirstName, LastName from users WHERE Username=%s and Password=%s'
     cur = mysql.connection.cursor()
@@ -31,11 +32,11 @@ def login():
     user = cur.fetchone()
     if user is not None and len(user) > 0:
         response_message = [True, 'You have successfully logged in', user]
-        return response_message
+        return jsonify(response_message)
 
     else:
         response_message = [False, 'No user was found with the given credentials']
-        return response_message
+        return jsonify(response_message)
 
 
 if __name__ == '__main__':
